02b3b66523ee8e22c73ef0a8cea26614b4ad3954
thoomaas31 <thomas.fourties@icloud.com>
2022-11-15 17:18:49 +0100

    🎨 app

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
+// Imports
 const express = require('express');
+const router = require('./router').router;
+
+// Instantiate server
 const app = express();
-const OpenApiValidator = require('express-openapi-validator');
 
-app.use(express.json());
-app.use(express.text());
-app.use(express.urlencoded({ extended: false }));
+// Body Parser configuration
+router.use(express.json());
+router.use(express.text());
+router.use(express.urlencoded({ extended: false }));
 
-const book = require('./controller/book');
+// Configure routes
+app.get('/', function (req, res) {
+  res.status(200).send('<h1>Nous rompishâmes</h1>');
+});
 
-// app.use(
-//   OpenApiValidator.middleware({
-//     apiSpec: './open-api.yaml',
-//     validateRequests: true,
-//   })
-// );
-
-app.use('/books', book);
-
-app.use((error, req, res, next) => {
+// Error config
+router.use((error, req, res, next) => {
   res
     .status(error.status || 500)
     .json({ success: false, message: error.message, errors: error.errors });
