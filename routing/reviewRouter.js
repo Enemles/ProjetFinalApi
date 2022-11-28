318dda1696d5f120a364617f20c03f97b2dcf6b8
thoomaas31 <thomas.fourties@icloud.com>
2022-11-28 22:19:40 +0100

    🔨 review Ctrl

--- routing/reviewRouter.js
+++ routing/reviewRouter.js
@@ -3,10 +3,10 @@ const reviewCtrl = require('../controller/reviewCtrl');
 const router = express.Router();
 
 router.get('/', reviewCtrl.getReviews);
-router.post('/', reviewCtrl.addReview);
+// router.post('/', reviewCtrl.addReview);
 router.delete('/reviewId', reviewCtrl.delReview);
 router.get('/reviewId', reviewCtrl.getReviewById);
-router.post('/like/:id', reviewCtrl.Like);
-router.post('/dislike/:id', reviewCtrl.Dislike);
+// router.post('/like/:id', reviewCtrl.Like);
+// router.post('/dislike/:id', reviewCtrl.Dislike);
 
 module.exports = router;
