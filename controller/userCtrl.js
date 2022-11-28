232efb68f2efa554ad8b3c661627ad40672bc1b9
jules <jules.bloux@etu.uca.fr>
2022-11-28 21:45:10 +0100

    getUserCurrent dans userCtrl permet de récupérer l utilisateur courant afin de l afficher dans le path /users/

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,27 +1,39 @@
 const userService = require("../services/user.js");
 const reviewService = require("../services/review");
+const cache = require("../caching/caching");
 
 module.exports = {
-  getUsers : async (req, res) => {
-  const listUsers = await userService.getUsers();
+  getUsers: async (req, res) => {
+    const listUsers = await userService.getUsers();
     res.json({ success: true, data: listUsers });
   },
-  getUserById : async (req, res) => {
+  getUserById: async (req, res) => {
     const username = parseInt(req.params.id);
     const user = await userService.getUserById(username);
     const reviews = reviewService.getReviewByUsername(username);
     if (user && user.lenngth === 1) {
       res.json({ success: true, header: user, data: reviews });
     }
-  },  
-  delUser : async (req, res) => {
+  },
+
+  getCurrentUser: async (req, res) => {
+    const currentUser = cache.getCachedValue(currentUser);
+    if (!currentUser) {
+      throw new error("Problème de mise en cache de l'utilisateur");
+    }
+    const user = userService.getUserById(currentUser);
+    const reviews = reviewService.getReviewByUsername(currentUser);
+    if (user && user.lenngth === 1) {
+      res.json({ success: true, header: user, data: reviews });
+    }
+  },
+
+  delUser: async (req, res) => {
     const username = parseInt(req.params.id);
     await userService.delUser(username);
     const user = await userService.getUserById(username);
     if (!user) {
-      res.json({ success: true, description: 'User has been deleted' });
+      res.json({ success: true, description: "User has been deleted" });
     }
-  }
-}
-
-
+  },
+};
