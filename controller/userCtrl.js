5e814776a23c6328a37c812348b0bab6f78b1842
Enemles <enemles.ihraf@gmail.com>
2022-11-28 21:05:06 +0100

    user controller fix syntax

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,25 +1,29 @@
 const userService = require('../services/user.js');
+const models = require('../models');
+
 const reviewService = require('../services/review');
 
-exports.getUsers = async (req, res) => {
+module.exports = {
+  getUsers : async (req, res) => {
   const listUsers = await userService.getUsers();
-  res.json({ success: true, data: listUsers });
-};
-
-exports.getUserById = async (req, res) => {
-  const username = parseInt(req.params.id);
-  const user = await userService.getUserById(username);
-  const reviews = reviewService.getReviewByUsername(username);
-  if (user && user.lenngth === 1) {
-    res.json({ success: true, header: user, data: reviews });
+    res.json({ success: true, data: listUsers });
+  },
+  getUserById : async (req, res) => {
+    const username = parseInt(req.params.id);
+    const user = await userService.getUserById(username);
+    const reviews = reviewService.getReviewByUsername(username);
+    if (user && user.lenngth === 1) {
+      res.json({ success: true, header: user, data: reviews });
+    }
+  },  
+  delUser : async (req, res) => {
+    const username = parseInt(req.params.id);
+    await userService.delUser(username);
+    const user = await userService.getUserById(username);
+    if (!user) {
+      res.json({ success: true, description: 'User has been deleted' });
+    }
   }
-};
+}
 
-exports.delUser = async (req, res) => {
-  const username = parseInt(req.params.id);
-  await userService.delUser(username);
-  const user = await userService.getUserById(username);
-  if (!user) {
-    res.json({ success: true, description: 'User has been deleted' });
-  }
-};
+
