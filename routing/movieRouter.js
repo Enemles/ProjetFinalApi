aa6125bdffe92ef4ce6cd892f64e473fe79d87d5
Enemles <enemles.ihraf@gmail.com>
2022-12-06 14:40:05 +0100

    🔒️ Security update on movieRouter

--- routing/movieRouter.js
+++ routing/movieRouter.js
@@ -7,7 +7,7 @@ authMidd = require("../middleware/auth");
 
 let cache = apicache.middleware;
 
-router.get("/", movieCtrl.getMovies);
-router.get("/toprated", authMidd.verifyAdmin, cache("20 minutes"), movieCtrl.getTopRated);
+router.get("/", authMidd.verifyAuthentication, movieCtrl.getMovies);
+router.get("/toprated", authMidd.verifyAuthentication, cache("20 minutes"), movieCtrl.getTopRated);
 
 module.exports = router;
