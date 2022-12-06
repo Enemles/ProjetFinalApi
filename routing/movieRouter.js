b2d4b4a79fe8b94c4cf691435844b68cbc7fdb74
Enemles <enemles.ihraf@gmail.com>
2022-12-06 19:45:12 +0100

    🔥 remove middleware redondant

--- routing/movieRouter.js
+++ routing/movieRouter.js
@@ -2,12 +2,11 @@ const apicache = require('apicache');
 const express = require("express");
 router = express.Router();
 movieCtrl = require("../controller/movieCtrl");
-authMidd = require("../middleware/auth");
 
 
 let cache = apicache.middleware;
 
-router.get("/", authMidd.verifyAuthentication, movieCtrl.getMovies);
-router.get("/toprated", authMidd.verifyAuthentication, cache("20 minutes"), movieCtrl.getTopRated);
+router.get("/", movieCtrl.getMovies);
+router.get("/toprated", cache("20 minutes"), movieCtrl.getTopRated);
 
 module.exports = router;
