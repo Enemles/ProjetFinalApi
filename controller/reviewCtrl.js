5a706a052d8c92f4416f0fcd92321884789a88dc
Enemles <enemles.ihraf@gmail.com>
2022-12-06 18:29:43 +0100

    ✨ add review + status code

--- controller/reviewCtrl.js
+++ controller/reviewCtrl.js
@@ -6,25 +6,28 @@ const models = require("../models");
 module.exports = {
   getReviews: async (req, res) => {
     const listReviews = await reviewService.getReviews();
-    res.json({ success: true, data: listReviews });
+    res.status(200).json({ success: true, data: listReviews });
   },
   addReview: async (req, res) => {
     try {
-      const { title, note, comment, moviename } = req.body;
+      const { cookies } = req;
+      const currentUser = cookies.username;
+      const { reviewtitle, rating, comment, moviename } = req.body;
+      const ratingString = rating.toString();
 
-      if (!(title && note && comment && moviename)) {
-        res.status(400).send("All input are required");
+      if (!(reviewtitle && ratingString && comment && moviename)) {
+        res.status(400).json({
+          success: false,
+          message : "All input are required"
+        });
       }
       if (!movieService.getMovieByMoviename(moviename)) {
         movieService.addMovie(moviename);
       }
 
-      const review = await reviewService.addReview( title, note, comment, moviename );
+      const review = await reviewService.addReview(currentUser, reviewtitle, rating, comment, moviename );
 
-      res.status(201).json({
-        success: "true",
-        review
-      });
+      res.status(201).json({success: "true", data: review});
     } catch (err) {
       console.log(err);
     }
@@ -32,37 +35,43 @@ module.exports = {
   delReview: async (req, res) => {
     const reviewId = req.params.reviewId;
     const data = await reviewService.delReview(reviewId);
+
     if (data) {
-      res.json({ success: true, description: 'Review has been deleted' });
+      res.status(200).json({ success: true, data: 'Review has been deleted' });
     }else{
-      res.json({ success: false, description: 'Review doesn\'t exist' });
+      res.status(404).json({ success: false, message: 'Review doesn\'t exist' });
     }
   },
   getReviewById: async (req, res) => {
     const reviewId = req.params.reviewId;
     const data = await reviewService.getReviewById(reviewId);
+
     if (data) {
-      res.json({ success: true, data });
+      res.status(200).json({ success: true, data : data });
     }else {
-      res.json({ success: false, description: 'Review doesn\'t exist' });
+      res.status(404).json({ success: false, message: 'Review doesn\'t exist' });
 
     }
   },
   Like: async (req, res) => {
     const reviewId = req.params.reviewId;
+
     try {
       const review = await reviewService.addLikeOnReview(reviewId);
-      res.json({ success: true, description: 'Like added' });
+      res.status(201).json({ success: true, description: 'Like added' });
     } catch (error) {
+      res.status(404).json({ success: false, message: 'Review not found' });
       console.log(error)
     }
   },
   Dislike: async (req, res) => {
     const reviewId = req.params.reviewId;
+
     try {
       const review = await reviewService.deleteLikeOnReview(reviewId);
-      res.json({ success: true, description: 'Dislike added' });
+      res.status(201).json({ success: true, description: 'Dislike added' });
     } catch (error) {
+      res.status(404).json({ success: false, message: 'Review not found' });
       console.log(error)
     }
   },
